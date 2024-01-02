6a5517372f889762e391f304a48516041cf909a6
prabesh Raj Pant <prabeshraj.211223@ncit.edu.np>
2024-01-02 15:52:15 +0545

    database connection

--- project/settings.py
+++ project/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Ecommerce',
+        'USER': 'prabesh',
+        'PASSWORD': 'Nshdya@123',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
