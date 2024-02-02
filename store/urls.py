e3fb6073e1199ac0fc25320337dfc289a510f474
prabesh Raj Pant <prabeshraj.211223@ncit.edu.np>
2024-02-02 13:02:32 +0545

    class based view

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('categories',category_list),
-    path('categories/<pk>',category_detail),
+    path('categories',CategoryList.as_view()),
+    path('categories/<pk>',CategoryDetails.as_view()),
 ]
--- store/views.py
+++ store/views.py
@@ -1,25 +1,86 @@
+from django.http import Http404
 from django.shortcuts import render,get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
 from rest_framework import status
+from rest_framework.views import APIView
 # Create your views here.
 
-@api_view(['GET','POST'])
-def category_list(request):
-    if request.method=='GET':
-        categories=Category.objects.all()
-        serializer=CategorySerializer(categories,many=True)
-        return Response(serializer.data)
-    else:
-        serializer=CategorySerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({
-          'error':"error in creating new category",  
-        },
-        status=status.HTTP_201_CREATED)
+# class base view
+class CategoryList(APIView):
+  def get(self,request):
+    categories=Category.objects.all()
+    serializer=CategorySerializer(categories,many=True)
+    return Response(serializer.data)
+  
+  def post(self,request):
+    serializer=CategorySerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response({
+      'error':"error in creating new category",  
+    },
+    status=status.HTTP_201_CREATED)
+  
+
+
+
+
+class CategoryDetails(APIView):
+  def get_object(self,pk):
+    try:
+      return Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+      raise Http404
+  
+  def get(self,request,pk):
+    category=self.get_object(pk)
+    serializer=CategorySerializer(category)
+    return Response(
+        serializer.data,
+      )
+    
+  def delete(self,request,pk):
+    category=self.get_object(pk)
+    category.delete()
+    return Response(
+        status=status.HTTP_204_NO_CONTENT
+      )
+    
+  def put(self,request,pk):
+    category=self.get_object(pk)
+    serializer=CategorySerializer(category,data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(
+        {
+          'details':'data has been updated'
+        }
+      )
+
+
+
+
+
+# functional base view...........
+
+
+# @api_view(['GET','POST'])
+# def category_list(request):
+#     if request.method=='GET':
+#       categories=Category.objects.all()
+#       serializer=CategorySerializer(categories,many=True)
+#       return Response(serializer.data)
+#     else:
+#       serializer=CategorySerializer(data=request.data)
+#       serializer.is_valid(raise_exception=True)
+#       serializer.save()
+#       return Response({
+#         'error':"error in creating new category",  
+#       },
+#       status=status.HTTP_201_CREATED)
  
  
 # TO BACKUP
@@ -28,25 +89,25 @@ def category_list(request):
 # python3 manage.py loaddata backup.json
  
         
-@api_view(['GET','DELETE','PUT'])     
-def category_detail(request,pk):
-    category=get_object_or_404(Category,pk=pk)
-    if request.method=="GET": 
-      serializer=CategorySerializer(category)
-      return Response(
-        serializer.data,
-      )
-    if request.method=="DELETE":
-      category.delete()
-      return Response(
-        status=status.HTTP_204_NO_CONTENT
-      )
-    if request.method=="PUT":
-      serializer=CategorySerializer(category,data=request.data)
-      serializer.is_valid(raise_exception=True)
-      serializer.save()
-      return Response(
-        {
-          'details':'data has been updated'
-        }
-      )
+# @api_view(['GET','DELETE','PUT'])     
+# def category_detail(request,pk):
+#     category=get_object_or_404(Category,pk=pk)
+#     if request.method=="GET": 
+#       serializer=CategorySerializer(category)
+#       return Response(
+#         serializer.data,
+#       )
+#     if request.method=="DELETE":
+#       category.delete()
+#       return Response(
+#         status=status.HTTP_204_NO_CONTENT
+#       )
+#     if request.method=="PUT":
+#       serializer=CategorySerializer(category,data=request.data)
+#       serializer.is_valid(raise_exception=True)
+#       serializer.save()
+#       return Response(
+#         {
+#           'details':'data has been updated'
+#         }
+#       )
